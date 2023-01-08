c0b8a89efa4655be96ac78d11a2a46d75e99688b
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2023-01-07 21:00:00 -0500

    Fix enemy bullet collider

--- JeuxUnderDogs/Assets/Scripts/EnemyBullet.cs
+++ JeuxUnderDogs/Assets/Scripts/EnemyBullet.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
-{   
+{
+    public int damage = 50;
+
     void Update()
     {
         if (!GetComponent<Renderer>().isVisible)
