8e7f6d7671ec7f3a87451dc21655edaf8eb80dde
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2023-01-08 14:01:53 -0500

    Healthbar + Fix messages erreurs

--- JeuxUnderDogs/Assets/HealthDisplay.cs
+++ JeuxUnderDogs/Assets/HealthDisplay.cs
@@ -11,21 +11,15 @@ public class HealthDisplay : MonoBehaviour
     public Image[] hearts;
 
     //Linking PlayerHealth script
-    public PlayerHealth playerHealth; 
-    
+    public Player player; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         //Make health and maxHealth same as in PlayerHealth script
-        health = playerHealth.health;
-        maxHealth = playerHealth.maxHealth;
+        health = player.health;
+        maxHealth = player.maxHealth;
 
         for (int i = 0; i < hearts.Length; i++)
         {
